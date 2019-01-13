98a3cc677f485d567225038fb6e704278a3f196b
Evoque <rubievoque@gmail.com>
2019-01-13 15:31:49 +0800

    finish: Login & Auth

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,7 +7,7 @@ const apiMall = 'https://sujiefs.com';
 /**
  *  WeChat **jscode**读取**sessionKey**
  */
-const wxJsCode2Session = params => wxRequest(params, `${apiMall}/api/wechat/jscode2session`);
+const wxJsCode2Session = params => wxRequest(`${apiMall}/api/wechat/jscode2session`, params);
 
 export {
     wxJsCode2Session
